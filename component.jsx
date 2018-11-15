3fee3ba771049fa31e368ec176918b7e44740641
Rafael <r.tomaszewski.a5@gmail.com>
2018-11-15 11:29:07 +0100

    Initial

---
+++ component.jsx
@@ -0,0 +1,41 @@
+// require('../css/style.css');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            width: '100px',
+            background: 'green'
+        }
+    }
+
+    handleEvent = () => {
+        this.setState({
+            background: 'yellow'
+        })
+    }
+
+    render() {
+        return (
+
+        )
+    }
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                color: 'red'
+            })
+        }, 3000)
+    }
+
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    ReactDOM.render(
+        <App/>,
+        document.getElementById('app')
+    );
+});
